fa4616121a02c05d3a238b0b1ecef4d5ab9c2870
Charles Wang <czw@seas.upenn.edu>
2024-08-17 22:34:15 -0700

    feat(header): update, rm tw-merge, cn

--- src/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// copied straight from shadcn/ui... thanks shadcn
-const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
-
-export { cn };
