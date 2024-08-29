ffe621bfc9b2bc38d0f6af3db266517138f98851
Charles Wang <czw@seas.upenn.edu>
2024-08-29 01:39:30 -0400

    feat(/games): basically finish, use builtin image() helper in config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,40 +12,41 @@ const board = defineCollection({
 
 const games = defineCollection({
   type: "content",
-  schema: z.object({
-    name: z.string(),
-    game: z.object({
-      type: z.enum(["club", "student"]),
-      jam: z.object({ semester: z.enum(["fall", "spring"]), year: z.number().int() }).optional(),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      game: z.object({
+        type: z.enum(["club", "student"]),
+        jam: z.object({ semester: z.enum(["fall", "spring"]), year: z.number().int() }).optional(),
+      }),
+      images: z.array(z.object({ file: image(), alt: z.string() })),
+      released: z.date(),
+      duration: z.string(),
+      links: z.array(z.object({ name: z.string(), href: z.string().url() })),
+      published: z.object({
+        itch: z
+          .object({
+            embedHref: z
+              .string()
+              .url()
+              .refine((url) => url.startsWith("https://itch.io/embed-upload/")),
+            width: z.number().int(),
+            height: z.number().int(),
+          })
+          .optional(),
+        steam: z
+          .object({
+            href: z
+              .string()
+              .url()
+              .refine((url) => url.startsWith("https://store.steampowered.com/")),
+          })
+          .optional(),
+      }),
+      credits: z.array(
+        z.union([z.string(), z.object({ role: z.string(), names: z.array(z.string()) })]),
+      ),
     }),
-    images: z.array(z.object({ file: z.string(), alt: z.string() })),
-    released: z.date(),
-    duration: z.string(),
-    links: z.array(z.object({ name: z.string(), href: z.string().url() })),
-    published: z.object({
-      itch: z
-        .object({
-          embedHref: z
-            .string()
-            .url()
-            .refine((url) => url.startsWith("https://itch.io/embed-upload/")),
-          width: z.number().int(),
-          height: z.number().int(),
-        })
-        .optional(),
-      steam: z
-        .object({
-          href: z
-            .string()
-            .url()
-            .refine((url) => url.startsWith("https://store.steampowered.com/")),
-        })
-        .optional(),
-    }),
-    credits: z.array(
-      z.union([z.string(), z.object({ role: z.string(), names: z.array(z.string()) })]),
-    ),
-  }),
 });
 
 export const collections = {
